9f2ed8edc04933512cf8c1a37fa1b8f8c0304793
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-11-05 04:06:32 -0300

    aula 08

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,9 @@
 class Position < ApplicationRecord
   belongs_to :company
+
+  enum careers: [:developer, :business_intelligence, :information_technology, :design, :product, :technology, :other]
+  enum contracts: [:clt, :pj, :match]
+
+  validates :name, :career, :contract, :city, :state, :summary, presence: true
+
 end
