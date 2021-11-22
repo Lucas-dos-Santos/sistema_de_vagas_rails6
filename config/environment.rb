aaf0f4396bffd5d30483ab56995f0627b9aa224f
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-11-22 16:23:14 -0300

    hide api key and fix position.summary

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative "application"
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+  :password => ENV['SENDGRID_API'], # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
