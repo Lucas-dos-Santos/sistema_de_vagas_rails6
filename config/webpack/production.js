1fdc14ab38d623e13f0ca4203856431bf5c616eb
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-11-22 16:29:18 -0300

    fix jquery

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  })
+)
+
 
 module.exports = environment.toWebpackConfig()
